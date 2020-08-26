5e7804215883ca28931676417aeca623fac8ff72
Jakub Mraz <jaku0899@edu.easj.dk>
2020-08-26 12:47:09 +0200

    Added the student class

---
+++ ClassRoom/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    class Student
+    {
+        public Student(string name, int birthmonth, int birthday)
+        {
+            Name = name;
+            BirthMonth = birthmonth;
+            Birthday = birthday;
+        }
+
+        public string Name { get; }
+        public int BirthMonth { get; }
+        public int Birthday { get; }
+    }
+}
