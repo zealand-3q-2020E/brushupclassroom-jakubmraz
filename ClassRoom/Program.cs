cc48dfef60bc3d91af7db793616591190ebda1c4
Jakub Mraz <jaku0899@edu.easj.dk>
2020-08-26 13:04:46 +0200

    Added code to print out the classroom information in the Main method

--- ClassRoom/Program.cs
+++ ClassRoom/Program.cs
@@ -13,6 +13,7 @@ namespace ClassRoom
             Classroom classroom1 = new Classroom();
             classroom1.ClassName = "3Q";
             classroom1.SemesterStart = new DateTime(2019, 08, 26);
+            classroom1.ClassList = new List<Student>();
 
             Student jakub = new Student("Jakub", 1, 12);
             Student katerina = new Student("Kateřina", 5, 25);
@@ -21,6 +22,14 @@ namespace ClassRoom
             classroom1.ClassList.Add(jakub);
             classroom1.ClassList.Add(katerina);
             classroom1.ClassList.Add(rania);
+
+            Console.WriteLine($"Classroom name: {classroom1.ClassName}; Start date: {classroom1.SemesterStart.ToString()}\n");
+            foreach (var student in classroom1.ClassList)
+            {
+                Console.WriteLine($"Student name: {student.Name}; Student's birthday: {student.Birthday}.{student.BirthMonth}.");
+            }
+
+            Console.ReadKey();
         }
     }
 }
