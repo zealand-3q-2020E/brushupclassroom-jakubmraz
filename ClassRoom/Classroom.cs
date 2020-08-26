e7abc54b7269962baca506426b6e34874dc2218b
Jakub Mraz <jaku0899@edu.easj.dk>
2020-08-26 12:51:12 +0200

    Added the Classroom class

---
+++ ClassRoom/Classroom.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    class Classroom
+    {
+        public Classroom()
+        {
+
+        }
+
+        public string ClassName { get; }
+        public List<Student> ClassList { get; }
+        public DateTime SemesterStart { get; }
+    }
+}
